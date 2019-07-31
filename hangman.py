5e826cdb328e7b8f6f82109fa6b0afee535c64fe
apayne <ashleigh.payne@adtran.com>
2019-07-31 12:27:50 -0500

    commit often?

--- hangman.py
+++ hangman.py
@@ -4,20 +4,22 @@
     #irc?
 
 #GLOBAL VARIABLE string of underscores / chars
+wordRemoving = ""
 word = ""
-wordWorking = ""
+wordWorking = "_"
 wordLength = 0
+usedChars = ""
 hangmanEmpty = "   ____\n  |    |\n  |\n  |\n  |\n  |\n__|__"
-hangman1 = "   ____\n  |    |\n  |    O\n  |\n  |\n  |\n__|__"
-hangman2 = "   ____\n  |    |\n  |    O\n  |    |\n  |\n  |\n__|__"
-hangman3 = "   ____\n  |    |\n  |    O\n  |   \|\n  |\n  |\n__|__"
-hangman4 = "   ____\n  |    |\n  |    O\n  |  '\|\n  |\n  |\n__|__"
-hangman5 = "   ____\n  |    |\n  |    O\n  |  '\|/\n  |\n  |\n__|__"
-hangman6 = "   ____\n  |    |\n  |    O\n  |  '\|/'\n  |\n  |\n__|__"
-hangman7 = "   ____\n  |    |\n  |    O\n  |  '\|/'\n  |   /\n  |\n__|__"
-hangman8 = "   ____\n  |    |\n  |    O\n  |  '\|/'\n  |  _/\n  |\n__|__"
-hangman9 = "   ____\n  |    |\n  |    O\n  |  '\|/'\n  |  _/ \\\n  |\n__|__"
-hangman10 = "   ____\n  |    |\n  |    O\n  |  '\|/'\n  |  _/ \\_\n  |\n__|__"
+hangman1 = "   ____\n  |    |\n  |   ( )\n  |\n  |\n  |\n__|__"
+hangman2 = "   ____\n  |    |\n  |   ( )\n  |    |\n  |\n  |\n__|__"
+hangman3 = "   ____\n  |    |\n  |   ( )\n  |   \|\n  |\n  |\n__|__"
+hangman4 = "   ____\n  |    |\n  |   ( )\n  |  '\|\n  |\n  |\n__|__"
+hangman5 = "   ____\n  |    |\n  |   ( )\n  |  '\|/\n  |\n  |\n__|__"
+hangman6 = "   ____\n  |    |\n  |   ( )\n  |  '\|/'\n  |\n  |\n__|__"
+hangman7 = "   ____\n  |    |\n  |   ( )\n  |  '\|/'\n  |   /\n  |\n__|__"
+hangman8 = "   ____\n  |    |\n  |   ( )\n  |  '\|/'\n  |  _/\n  |\n__|__"
+hangman9 = "   ____\n  |    |\n  |   ( )\n  |  '\|/'\n  |  _/ \\\n  |\n__|__"
+hangman10 = "   ____\n  |    |\n  |   ( )\n  |  '\|/'\n  |  _/ \\_\n  |\n__|__"
 
 #GLOBAL VARIABLE CONST max amount of guesses (how many parts does mr hangman have)
 allowedWrongGuesses = 10 #head, body, arms, hands, legs, feet
@@ -31,66 +33,80 @@ continuePlaying = True
 
 #display opening message function
 def openingMessage():
-    print "Hello! Welcome to Hangman. Preparing man to be hung...\nDone!\n\nPreparing word...\nDone!"
+    print("Hello! Welcome to Hangman. Preparing man to be hung...\nDone!\n\nPreparing word...\nDone!")
     
-    print "Hope you're ready, here we go!\n\n"
+    print("Hope you're ready, here we go!\n\n")
 
 #pick word function
 def pickWord():
 
 #generate array of underscores function
 def generateString():
+    i = 1
+    while (i < len(word)):
+        wordWorking = wordWorking + " _ "
 
 #play game function (wait for user input and bus it out somewhere else)
 def playGame():
-    pickWord
+    pickWord()
+    generateString()
     while(continuePlaying):
 
-        showGame
+        showGame()
         userInput = input("Guess a letter: ")
+        if (userInput in usedChars):
+            continue
+
         inputValid = checkInput(userInput)
+        usedChars = usedChars = ", " userInput
 
-        if inputValid:
-            correctInput
-        else:
-            incorrectInput
+        if(inputValid & rightGuesses < len(word)):
+            correctInput(userInput)
+        elif(inputValid & rightGuesses == len(word)):
+            theyWin()
+        elif(not inputValid & wrongGuesses <= allowedWrongGuesses):
+            incorrectInput(userInput)
+        elif(not inputValid & wrongGuesses > allowedWrongGuesses):
+            theyLose()
 
-    playAgain
+    playAgain()
 
 #check if input is in word
 def checkInput(userInput):
+    return (userInput in wordRemoving)
 
 #show game board
 def showGame():
-    print wordWorking
-    
+    print(wordWorking)
+    print("\nCharacters guessed: " usedChars)
+
     if(wrongGuesses == 0):
-        print hangmanEmpty
+        print(hangmanEmpty)
     elif(wrongGuesses == 1):
-        print hangman1
+        print(hangman1)
     elif(wrongGuesses == 2):
-        print hangman2
+        print(hangman2)
     elif(wrongGuesses == 3):
-        print hangman3
+        print (hangman3)
     elif(wrongGuesses == 4):
-        print hangman4
+        print (hangman4)
     elif(wrongGuesses == 5):
-        print hangman5
+        print (hangman5)
     elif(wrongGuesses == 6):
-        print hangman6
+        print (hangman6)
     elif(wrongGuesses == 7):
-        print hangman7
+        print (hangman7)
     elif(wrongGuesses == 8):
-        print hangman8
+        print (hangman8)
     elif(wrongGuesses == 9):
-        print hangman9
+        print (hangman9)
     elif(wrongGuesses == 10):
-        print hangman10
+        print (hangman10)
 
 #user gave correct input, update string
 def correctInput(userInput):
     rightGuesses = rightGuesses + 1
-    print "Right! :D Your guess " userInput " is in the word."
+    print ("Right! :D Your guess " userInput " is in the word.")
 
 #user gave last correct input, they win
 def theyWin():
@@ -99,11 +115,11 @@ def theyWin():
 #user gave incorrect input, edit hangman and incorrect guesses
 def incorrectInput(userInput):
     wrongGuesses = wrongGuesses + 1
-    print "Wrong >:( Your guess " userInput " is not in the word.\nMr. Hangman gets another body part."
+    print ("Wrong >:( Your guess " userInput " is not in the word.\nMr. Hangman gets another body part.")
     
     remaining = allowedWrongGuesses - wrongGuesses
     if(remaining <= 4):
-        print "Warning!!! Only " remaining " wrong guesses remaining!"
+        print ("Warning!!! Only " remaining " wrong guesses remaining!")
 
 #user maxed out guesses, they lose
 def theyLose():
@@ -114,12 +130,12 @@ def theyLose():
 def playAgain():
     play = input("Do you want to play again? (y/n): ")
     if (play == "y" || play == "Y"):
-        print "Yay!! Here we go again!!!!!"
+        print ("Yay!! Here we go again!!!!!")
         playGame
     else:
-        print "Baiiiii"
+        print ("Baiiiii")
 
 
 #dont need a main
-openingMessage
-playGame
+openingMessage()
+playGame()
