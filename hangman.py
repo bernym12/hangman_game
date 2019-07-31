8b5778708e848393510396f1c873e47a9dadc0eb
apayne <ashleigh.payne@adtran.com>
2019-07-31 09:57:39 -0500

    stubbed comments

---
+++ hangman.py
@@ -0,0 +1,36 @@
+#an attempt at making a hangman game in my last 3 days at adtran!
+#QUESTIONS
+    #categories? or just words???
+
+#GLOBAL VARIABLE string of underscores / chars
+str word = ""
+
+#GLOBAL VARIABLE CONST max amount of guesses (how many parts does mr hangman have)
+CONST int = 10 #head, body, arms, hands, legs, feet
+
+#GLOBAL VARIABLE num of incorrect guesses
+int wrongGuesses = 0
+
+#display opening message function
+def openingMessage:
+    
+
+#pick word function
+
+#generate array of underscores function
+
+#play game function (wait for user input and bus it out somewhere else)
+
+#check if input is in word
+
+#user gave correct input, update string
+
+#user gave last correct input, they win
+
+#user gave incorrect input, edit hangman and incorrect guesses
+
+#user maxed out guesses, they lose
+
+#play again?
+
+#main
