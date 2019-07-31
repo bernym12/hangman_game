32b8cc7a313961d5dc9b3d762fb35b14acf66730
apayne <ashleigh.payne@adtran.com>
2019-07-31 10:11:23 -0500

    named fucntions and basic play game

--- hangman.py
+++ hangman.py
@@ -3,34 +3,65 @@
     #categories? or just words???
 
 #GLOBAL VARIABLE string of underscores / chars
-str word = ""
+word = ""
+wordLength = 0
 
 #GLOBAL VARIABLE CONST max amount of guesses (how many parts does mr hangman have)
-CONST int = 10 #head, body, arms, hands, legs, feet
+allowedGuesses = 10 #head, body, arms, hands, legs, feet
 
 #GLOBAL VARIABLE num of incorrect guesses
-int wrongGuesses = 0
+wrongGuesses = 0
+
+#GLOBAL VARIABLE keep running game
+continuePlaying = True
 
 #display opening message function
 def openingMessage:
+    print "Hello! Welcome to Hangman. Preparing man to be hung...\nDone!\n\nPreparing word...\nDone!"
     
+    print "Hope you're ready, here we go!"
 
 #pick word function
+def pickWord:
 
 #generate array of underscores function
+def generateString:
 
 #play game function (wait for user input and bus it out somewhere else)
+def playGame:
+    pickWord
+    while(continuePlaying):
+
+        userInput = getInput
+        inputValid = checkInput(userInput)
+
+        if inputValid:
+            correctInput
+        else:
+            incorrectInput
+
+#get input from player
+def getInput:
 
 #check if input is in word
+def checkInput:
 
 #user gave correct input, update string
+def correctInput:
 
 #user gave last correct input, they win
+def theyWin:
+    continuePlaying = false
 
 #user gave incorrect input, edit hangman and incorrect guesses
+def incorrectInput:
 
 #user maxed out guesses, they lose
+def theyLose:
+    continuePlaying = false
+
 
 #play again?
+def playAgain:
 
-#main
+#dont need a main
